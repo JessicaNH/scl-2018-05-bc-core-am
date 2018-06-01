5447398cc9e4e2f592d7b3aaf5438e467b45cc1c
Jessica Navarro <jess.nav359.herrera@gmail.com>
2018-06-01 09:48:12 -0400

    mejorando la pagina

--- src/cipher.js
+++ src/cipher.js
@@ -1,43 +1,43 @@
 window.cipher = {
   
    
-  encode: (text, offset) => { 
+  encode: (string, offset) => { 
      /* Acá va tu código */
     
-    let cifradas = ''; // declarando variable cifrada
-    for(let i = 0; i < text.length; i++){
-        //var i = 0  :  i va ser cada letra de las palabras y va a mpezar en 0 
-        // text.length es el largo que tenga las pabalabras
-        // i++  : va ir sumando de a 1 
+    let cifrado = ''; // declarando variable cifrado
+    for(let i = 0; i < string.length; i++){
+
         
-      let ascci = (text.toUpperCase().charCodeAt(i));
-      let formula = (text.charCodeAt(i)- 65 + parseInt(offset)) % 26 + 65;
+       let ascci = string.toUpperCase();
+
+       let formula = (ascci.charCodeAt(i)- 65 + parseInt(offset)) % 26 + 65;
         //capturar las letras mayusculas y el codigo ascci  
-      cifradas += String.fromCharCode(formula);
-        // String.fromCharCode() es para convertir el numero es letra , osea va convertir el charCodeAt         //de arriba que me va dar numeros , en letras 
+       cifrado+= String.fromCharCode(formula);
+       
+              
       }
-      return cifradas;
-      console.log(cifradas);
-    //console.log  muestra las letras cifradas en la consola
+      return cifrado;
+      
+      console.log(cifrado);
+    
   },
   
      
-  decode: (string,offset) => {
+  decode: (string, offset) => {
     /* Acá va tu código */
-    function decipher(){
-      var cajaDecipher = document.getElementById('caja-decipher');
-      var propmt = prompt('pon una frase o palabra');
-      var dechiper = '';
-    // for para recorrer cada letra del las palabras cifradas
-      for(var j = 0; j < propmt.length; j++){
-        //lo mismo que esta escrito arriba
-          var formulaDechiper = (propmt.toUpperCase().charCodeAt(j) + 65 - 33) % 26 + 65;
-        //aca pongo al reves los simbolo de +65 y -33 , para descrifrar lo que ya habia cifrado
+    let decifrado = ''; 
+    for(let i = 0; i < string.length; i++){
+
         
-          var decifradas = String.fromCharCode(formulaDechiper);
-          dechiper+= decifradas;
+       let ascci = (string.toUpperCase().charCodeAt(i));
+       let formula = (string.charCodeAt(i)- 65 - parseInt(offset)) % 26 + 65;
+        //capturar las letras mayusculas y el codigo ascci  
+       decifrado += String.fromCharCode(formula);
+       
+              
       }
-      cajaDecipher.innerHTML = dechiper;
+      return decifrado;
+      
   }
-  
+}; 
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 /* Acá va tu código */
 
-let text = document.getElementById ("message").value;
-let offset = document.getElementById ("displace").value;
-let cajaCcipher = document.getElementById('caja-cipher');
-const button = document.getElementById ("cipher");
+const btn_cipher = document.getElementById ("cipher");
+const btn_decipher = document.getElementById ("decipher");
 let solution = document.getElementById ("root");
-button.addEventListener("click", () =>{
-  
+btn_cipher.addEventListener("click", () => {
+  let string = document.getElementById ("message").value;
+  let offset = document.getElementById ("displace").value;
+  let exit = window.cipher.encode(string, offset);
+   solution.innerHTML = exit;
 })
+btn_decipher.addEventListener("click", () => {
+  let string = document.getElementById ("message").value;
+  let offset = document.getElementById ("displace").value;
+  let exit = window.cipher.decode(string, offset);
+   solution.innerHTML = exit;
+ })
